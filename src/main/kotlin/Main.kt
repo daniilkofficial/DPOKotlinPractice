22bb28a03e23326373d4b4ff31bdedba0ebed194
DaniilK <daniilkamornicki@gmail.com>
2023-02-16 10:46:54 +0300

    Done work and fix names

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -17,9 +17,9 @@ fun main() {
         .forEach { println(it) }
 
     // Задание 6. Вывод уникальных номеров
-    val contactsSet = numbers.toSet()
-    println("\nУникальные номера (${contactsSet.size} шт.):")
-    contactsSet.forEach {
+    val numbersSet = numbers.toSet()
+    println("\nУникальные номера (${numbersSet.size} шт.):")
+    numbersSet.forEach {
         println(it)
     }
 
@@ -28,15 +28,15 @@ fun main() {
     println("\nСумма длин всех номеров: $sumLength\n")
 
     // Задание 8. Заполнение Map именами к номерам телефонов
-    val contactsAndName: MutableMap<String, String> = mutableMapOf() // номер, имя
-    contactsSet.forEach {
+    val numbersAndName: MutableMap<String, String> = mutableMapOf() // номер, имя
+    numbersSet.forEach {
         println("Введите имя для номера $it: ")
         val name = readln()
-        contactsAndName[it] = name // в ключ записывается имя
+        numbersAndName[it] = name // в ключ записывается имя
     }
 
     // Задание 9. Вывод всё содержание Map
-    contactsAndName.forEach { (number, name) ->
+    numbersAndName.forEach { (number, name) ->
         println("Человек: $name. Номер телефона: $number")
     }
 
@@ -44,12 +44,12 @@ fun main() {
 
 // Функция ввода номеров, добавления в список и возврат списка
 fun getNumbers(n: Int): MutableList<String> {
-    val contacts: MutableList<String> = MutableList(0) { index -> index.toString() }
+    val numbers: MutableList<String> = MutableList(0) { index -> index.toString() }
 
     for (i in 0 until n) {
         println("Введите номер телефона: ")
         val number = readlnOrNull() ?: "+7(918)366-24-24"
-        contacts.add(number)
+        numbers.add(number)
     }
-    return contacts
+    return numbers
 }
